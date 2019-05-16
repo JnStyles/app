749f3ce25739288d6ab8ef91443910d21ef41c93
jiangnan <13611266774@163.com>
2019-05-16 18:42:12 +0800

    首次提交

---
+++ src/components/Tabbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <tabbar>
+      <tabbar-item>
+        <img slot="icon" src="../assets/logo.png">
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src="../assets/logo.png">
+        <span slot="label">Message</span>
+      </tabbar-item>
+      <tabbar-item selected link="/component/demo">
+        <img slot="icon" src="../assets/logo.png">
+        <span slot="label">Explore</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/logo.png">
+        <span slot="label">News</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+  import { Tabbar, TabbarItem } from 'vux'
+
+  export default {
+    components: {
+      Tabbar,
+      TabbarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <x-header :left-options="{showBack: false}">do not show Back</x-header>
+    <group>
+      <cell title="title" value="value"></cell>
+      <cell title="title" value="121233"></cell>
+      <cell title="title" value="234"></cell>
+      <cell title="title" value="345"></cell>
+    </group>
+    <tabbar></tabbar>
+  </div>
+</template>
+
+<script>
+
+  import { Group, Cell,XHeader} from 'vux'
+  import tabbar from '../components/Tabbar'
+
+  export default {
+    data:function(){
+      return {
+
+      }
+    },
+    components: {
+      Group,
+      Cell,
+      tabbar,
+      XHeader
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
