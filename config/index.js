9d39ebbc8cd042a045c4b958f9a6a3ba84cda5bf
jiangnan <13611266774@163.com>
2019-06-21 19:11:44 +0800

    提交

--- config/index.js
+++ config/index.js
@@ -63,7 +63,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'static1',
     assetsPublicPath: './',
 
     /**
